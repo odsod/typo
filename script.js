4f62a29b8ea30e608bdc8e355c19f48b4eca3aff
Oscar Söderlund <poscar@spotify.com>
2013-11-06 14:09:06 +0100

    Initial commit
    Change-Id: I15d6f76ae64b5349f2762aff34a17eb8b07ead5d

---
+++ script.js
@@ -0,0 +1,45 @@
+var target = 'var foo = \'bar\';\nvar baz = \'booze\';\nif (blaz) {\n  return haz;\n}';
+
+var commonPrefixLength = function(lhs, rhs) {
+  var i = 0;
+  while (i < Math.min(lhs.length, rhs.length) && lhs[i] === rhs[i]) { ++i; }
+  return i;
+};
+
+var vm = {};
+
+vm.typed = ko.observable('');
+
+vm.hasTypedCharacterCorrectly = function(index) {
+  return ko.unwrap(index) < commonPrefixLength(vm.typed(), target);
+};
+
+vm.hasTypedCharacterIncorrectly = function(index) {
+  return false; // TODO
+};
+
+vm.isNextCharacterToBeTyped = function(index) {
+  return ko.unwrap(index) === vm.typed().length;
+};
+
+vm.chars = target.split('');
+
+ko.bindingHandlers.alwaysFocus = {
+  init: function(element) {
+    var blurHandler = function() {
+      setTimeout(function() { element.focus(); }, 0);
+    };
+    ko.utils.domData.set(element, 'alwaysFocusBlurHandler', blurHandler);
+  },
+  update: function(element, valueAccessor) {
+    var blurHandler = ko.utils.domData.get(element, 'alwaysFocusBlurHandler');
+    if (ko.unwrap(valueAccessor())) {
+      element.addEventListener('blur', blurHandler);
+      setTimeout(function() { element.focus(); }, 0);
+    } else {
+      element.removeEventListener('blur', blurHandler);
+    }
+  }
+};
+
+ko.applyBindings(vm);
