7e6943cddb9a7e63f296d854392ce6642a15e782
Oscar Söderlund <poscar@spotify.com>
2013-11-09 15:47:58 +0100

    Implement skippable characters
    Change-Id: I2aaa4f4a9de098526cb256c9d2cb71ee998b57c7

--- script.js
+++ script.js
@@ -1,28 +1,76 @@
-var target = 'var foo = \'bar\';\nvar baz = \'booze\';\nif (blaz) {\n  return haz;\n}';
+var fullText = 'var foo = \'bar\';\nvar baz = \'booze\';\n\nif (blaz) {\n  return haz;\n}';
 
-var commonPrefixLength = function(lhs, rhs) {
-  var i = 0;
-  while (i < Math.min(lhs.length, rhs.length) && lhs[i] === rhs[i]) { ++i; }
-  return i;
+var indicesToSkip = [];
+
+var skipInitialWhitespace = /^\s+/gm;
+
+var match;
+while (!!(match = skipInitialWhitespace.exec(fullText))) {
+  for (var i = 0; i < match[0].length; ++i) {
+    indicesToSkip[match.index + i] = true;
+  }
+}
+
+var UntypeableCharacter = function(fullText, indexInFulltext) {
+  this.character = fullText[indexInFulltext];
+  this.hasBeenTypedCorrectly = false;
+  this.hasBeenTypedIncorrectly = false;
+  this.isNextCharacterToBeTyped = false;
+  this.isReturn = false;
+};
+
+var TypeableCharacter = function(fullText, indexInFullText, typedText, indicesToSkip) {
+  var self = this;
+
+  var skippedCharactersBeforeMe = indicesToSkip.slice(0, indexInFullText + 1).reduce(function(total, skip) {
+    return total + (skip ? 1 : 0);
+  }, 0);
+
+  var indexInTypedText = indexInFullText - skippedCharactersBeforeMe;
+
+  this.hasBeenTyped = function() {
+    return ko.unwrap(typedText).length > indexInTypedText;
+  };
+
+  this.character = ko.computed(function() {
+    if (self.hasBeenTyped()) {
+      return ko.unwrap(typedText)[indexInTypedText];
+    } else {
+      return fullText[indexInFullText];
+    }
+  });
+
+  this.hasBeenTypedCorrectly = ko.computed(function() {
+    return ko.unwrap(typedText)[indexInTypedText] ===
+           fullText[indexInFullText];
+  });
+
+  this.hasBeenTypedIncorrectly = ko.computed(function() {
+    return self.hasBeenTyped() &&
+           ko.unwrap(typedText)[indexInTypedText] !==
+           fullText[indexInFullText];
+  });
+
+  this.isNextCharacterToBeTyped = ko.computed(function() {
+    return indexInTypedText === ko.unwrap(typedText).length;
+  });
+
+  this.isReturn = ko.computed(function() {
+    return self.character() === '\n';
+  });
 };
 
 var vm = {};
 
-vm.typed = ko.observable('');
+vm.typedText = ko.observable('');
 
-vm.hasTypedCharacterCorrectly = function(index) {
-  return ko.unwrap(index) < commonPrefixLength(vm.typed(), target);
-};
-
-vm.hasTypedCharacterIncorrectly = function(index) {
-  return false; // TODO
-};
-
-vm.isNextCharacterToBeTyped = function(index) {
-  return ko.unwrap(index) === vm.typed().length;
-};
-
-vm.chars = target.split('');
+vm.charactersInFullText = fullText.split('').map(function(_, indexInFullText) {
+  if (indicesToSkip[indexInFullText]) {
+    return new UntypeableCharacter(fullText, indexInFullText);
+  } else {
+    return new TypeableCharacter(fullText, indexInFullText, vm.typedText, indicesToSkip);
+  }
+});
 
 ko.bindingHandlers.alwaysFocus = {
   init: function(element) {
